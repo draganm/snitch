4eb6e8ee8845d2be175a0809b17d3d27be98fef8
Dragan Milic <dragan@netice9.com>
2017-08-16 22:59:50 +0100

    started adding delete target confirmation modal

--- js/screens/targetDetails.js
+++ js/screens/targetDetails.js
@@ -1,16 +1,29 @@
 var id
-
+var showDeleteConfirm = false
+var config = {}
 function mount() {
   id = ctx.Params["targetID"]
 	setTitle("Snitch: Target Details")
 	listen(dbpath("targets",id), function(r) {
-		var config = JSON.parse(r.Data(dbpath("config")))
-		render(config)
+		config = JSON.parse(r.Data(dbpath("config")))
+		render()
 	});
 }
 
 function onUserEvent(evt) {
+  if (evt.ElementID === "deleteButton") {
+    showDeleteConfirm = true
+    render()
+  }
 
+  if (evt.ElementID === "deleteConfirmButton") {
+
+  }
+
+  if (evt.ElementID === "deleteCancelButton" || evt.ElementID == "deleteConfirmModal") {
+    showDeleteConfirm = false
+    render()
+  }
 }
 
 
@@ -19,20 +32,14 @@ function unmount() {
 }
 
 
-function render(config) {
+function render() {
 	var dm = targetDetailsDisplay.DeepCopy()
   dm.SetElementAttribute("panel","header","Target "+config.name+" ("+id+")")
-	// console.log("read targets",targets)
-
-
-	// for (var i = 0; i<targets.length; i++) {
-	// 	var it = indexTarget.DeepCopy()
-	// 	var target = targets[i]
-	// 	it.SetElementAttribute("item","bsStyle",statusStyle(target.status))
-	// 	it.SetElementAttribute("item","href","#/targets/"+target.id)
-	// 	it.SetElementText("item", target.name)
-	// 	dm.AppendChild("targets", it)
-	// }
+  dm.SetElementText("image", config.image)
+  dm.SetElementText("command", config.command)
+  if (showDeleteConfirm) {
+    dm.ReplaceChild("deleteButton", confirmDeleteModal)
+  }
 	updateScreen(withNavigation(dm));
 }
 
